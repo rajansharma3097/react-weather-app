7e66a34667b6e9bc0766fb1b60f99de0ee705671
RJay Sharma <sharma.rajan3097@gmail.com>
2022-02-18 14:24:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Loader } from "semantic-ui-react";
+import WeatherCard from "./components/WeatherCard";
+import Header from "./components/Header";
+import Forecast from "./components/Forecast";
 
 function App() {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [city, setCity] = useState("");
+  const [temperature, setTemperature] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [sunrise, setSunrise] = useState(null);
+  const [sunset, setSunset] = useState(null);
+  const [icon, setIcon] = useState("");
+  const [forecast, setForecast] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+
+    const fetchData = async () => {
+      const currentWeather = await axios.get(
+        `${process.env.REACT_APP_API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+      );
+
+      const forecast = await axios.get(
+        `${process.env.REACT_APP_API_URL}/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minute&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+      );
+
+      setLoading(false);
+      setTemperature(currentWeather.data.main.temp);
+      setSunset(currentWeather.data.sys.sunset);
+      setSunrise(currentWeather.data.sys.sunrise);
+      setHumidity(currentWeather.data.main.humidity);
+      setCity(currentWeather.data.name);
+      setIcon(currentWeather.data.weather[0].main);
+      setForecast(forecast.data.daily);
+    };
+
+    fetchData();
+  }, [latitude, longitude]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Header />
+      {loading ? (
+        <div>
+          <p>Loading... Please Wait</p>
+          <Loader active inline="centered" />
+        </div>
+      ) : (
+        <WeatherCard
+          temperature={temperature}
+          humidity={humidity}
+          sunrise={sunrise}
+          sunset={sunset}
+          city={city}
+          icon={icon}
+        />
+      )}
+      <Forecast forcast={forecast} />
     </div>
   );
 }
